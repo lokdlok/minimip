c465d8840210028ed0c90d22fd780446e53c2751
lok <kevin.kevinleo@gmail.com>
2012-06-21 14:18:21 +0100

    Added some comments

--- lpsolver.cpp
+++ lpsolver.cpp
@@ -17,7 +17,7 @@ void printTableaux(double** rows, double* rhs, double* Z,
 }
 
 double* simplex(double** rows, double* rhs, double* Z,
-                     unsigned int nrows, unsigned int ncols){
+                unsigned int nrows, unsigned int ncols){
     double* cmz = new double[ncols];
     double* theta = new double[nrows];
     int* row_vars = new int[nrows];
@@ -30,10 +30,13 @@ double* simplex(double** rows, double* rhs, double* Z,
             if(rows[r][c] == 1.0) row_vars[r] = c;
 
     while(1) {
-
         pivot_variable = 0;
         pivot_row = 0;
 
+        /*
+         * Calculate the C - Z vector, the pivot variable is the j with maximum
+         * Cj - Zj.
+         */
         for(unsigned int c=0; c<ncols; c++) {
             double sum = 0.0;
             for(unsigned int r=0; r<nrows; r++) {
@@ -43,6 +46,10 @@ double* simplex(double** rows, double* rhs, double* Z,
             if(cmz[c] > cmz[pivot_variable]) pivot_variable = c;
         }
 
+        /* 
+         * When the pivot variable (max Cj-Zj) is 0 or negative we have found
+         * our solution
+         */
         if(cmz[pivot_variable] <= 0.0) {
             double* solution = new double[ncols];
             for(unsigned int r=0; r<nrows; r++)
@@ -50,6 +57,10 @@ double* simplex(double** rows, double* rhs, double* Z,
             return solution;
         }
 
+        /* 
+         * Calculate the theta vector, the result of moving the coefficient for
+         * the pivot variable in a row to the right hand side.
+         */
         for(unsigned int r=0; r<nrows; r++) {
             if(rows[r][pivot_variable] > 0) {
                 theta[r] = rhs[r] / rows[r][pivot_variable];
@@ -61,11 +72,23 @@ double* simplex(double** rows, double* rhs, double* Z,
         printTableaux(rows, rhs, Z, nrows, ncols, cmz, theta, row_vars);
 #endif
         
+        /*
+         * Update which variable represents the pivot row and divide the
+         * pivot row by the pivot element. The pivot element should go to
+         * 1.0.
+         */
         row_vars[pivot_row] = pivot_variable;
         pivot_element = rows[pivot_row][pivot_variable];
         for(unsigned int c=0; c<ncols; c++) rows[pivot_row][c] /= pivot_element;
         rhs[pivot_row] /= pivot_element;
 
+        /* 
+         * Update the rest of the coefficients. Since the pivot element is 1.0
+         * the rest of the coefficients for the pivot variable must be set to
+         * 0.0. To do this we mulitply the pivot row by the coefficient of the
+         * pivot variable in a non pivot row and then subtract the row from the
+         * pivot row.
+         */
         for(unsigned int r=0; r<nrows; r++) {
             double coef = rows[r][pivot_variable];
             if(r != pivot_row) {
@@ -73,7 +96,6 @@ double* simplex(double** rows, double* rhs, double* Z,
                 rhs[r] -= coef * rhs[pivot_row];
             }
         }
-
     }
 }
 
